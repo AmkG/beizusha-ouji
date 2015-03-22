ce003fd3fe434bf004cc98e7217e88e7d5d061e7
Alan Manuel K. Gloria <almkglor@gmail.com>
2015-03-23 05:54:23 +0800

    Initial commit.

---
+++ main.js
@@ -0,0 +1,74 @@
+/* main.js - main source.  */
+/**
+ *
+ * @licstart  The following is the entire license notice for the 
+ *  JavaScript code in this page.
+ *
+ * Copyright (C) 2015  Alan Manuel K. Gloria
+ *
+ *
+ * The JavaScript code in this page is free software: you can
+ * redistribute it and/or modify it under the terms of the GNU
+ * General Public License (GNU GPL) as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option)
+ * any later version.  The code is distributed WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU GPL for more details.
+ *
+ * As additional permission under GNU GPL version 3 section 7, you
+ * may distribute non-source (e.g., minimized or compacted) forms of
+ * that code without the copy of the GNU GPL normally required by
+ * section 4, provided you include this license notice and a URL
+ * through which recipients can access the Corresponding Source.
+ *
+ * @licend  The above is the entire license notice
+ * for the JavaScript code in this page.
+ *
+ */
+define([], function() {
+
+function main(PIXI, requestAnimFrame, document, window) {
+  // ignore mouse events.
+  function ignoreMouseEvent(e) {
+    e = e || window.event;
+    e.preventDefault();
+    e.stopPropagation();
+  }
+  window.addEventListener('mousedown', ignoreMouseEvent, true);
+
+  /* TODO: Initialize engine.  */
+  /* TODO: Initialize screens.  */
+  /* TODO: Indicate starting screen for engine.  */
+  /* TODO: Enter engine loop.  */
+}
+
+function initialize(PIXI, requestAnimFrame, document, window) {
+  var called = false;
+
+  function nullFun() {}
+  function onDOMContentLoaded() {
+    if (!called) {
+      called = true;
+      document.removeEventListener("DOMContentLoaded", onDOMContentLoaded, false);
+      window.removeEventListener("load", onDOMContentLoaded, false);
+      document.onreadystatechange = nullFun;
+      main(PIXI, requestAnimFrame, document, window);
+    }
+  }
+  function onreadystatechange() {
+    if (document.readyState === 'complete') {
+      onDOMContentLoaded();
+    }
+  }
+
+  if (document.readyState === 'complete') {
+    main(PIXI, requestAnimFrame, document, window);
+  } else {
+        document.addEventListener('DOMContentLoaded', onDOMContentLoaded, false);
+        window.addEventListener('load', onDOMContentLoaded, false);
+        document.onreadystatechange = onreadystatechange;
+  }
+}
+
+return initialize;
+});
